324359f46226e6cb4fd2d165129c7d6ccac605e8
Ajay D <ajay041822@gmail.com>
2025-12-11 16:59:43 +0530

    Delete challenges directory

--- challenges/challenge_1_sum_avg/sol.py
+++
@@ -1,6 +0,0 @@
-operand1 = float(input("Enter the first number: "))
-operand2 = float(input("Enter the second number: "))
-sum = operand1 + operand2
-avg = (operand1 + operand2)/2.0
-print("Sum is: "+ str(sum))
-print("Average is: " + str(avg))
